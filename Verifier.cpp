5af16476b5cf2e1f6145499ea2ccadf4a37fce22
Danila Usachev <duesna897@gmail.com>
2024-12-21 21:41:53 +0300

    only designation access left

--- Verifier.cpp
+++ Verifier.cpp
@@ -1,7 +1,7 @@
 #include "Verifier.h"
 #include "ByteFile.h"
-#include "Error.h"
 #include "Inst.h"
+#include "fmt/format.h"
 #include <assert.h>
 #include <cstdint>
 #include <memory>
@@ -19,15 +19,31 @@ template <typename... A>
       fmt::format(std::forward<decltype(args)>(args)...));
 }
 
+static constexpr int32_t II_REACHED = (1 << 0);
+
+static constexpr int32_t FI_REACHED = (1 << 0);
+static constexpr int32_t FI_IS_CLOSURE = (1 << 1);
+
 namespace {
 
-union InstInfo {
-  int32_t basicBlockNum;
-  struct {
-    char rift = false;
-    char visited = false;
-    int16_t operandStackSize;
-  };
+struct FunctionInfo {
+  int8_t flags = 0;
+  int16_t nclosurevars = 0;
+
+  bool isReached() const noexcept { return flags & FI_REACHED; }
+  void setReached() noexcept { flags |= FI_REACHED; }
+  bool isClosure() const noexcept { return flags & FI_IS_CLOSURE; }
+  bool isNonClosure() const noexcept { return !isClosure(); }
+  void setClosure() noexcept { flags |= FI_IS_CLOSURE; }
+  void setNonClosure() noexcept { flags &= ~FI_IS_CLOSURE; }
+};
+
+struct InstInfo {
+  int8_t flags = 0;
+  int16_t operandStackSize;
+
+  bool isReached() const noexcept { return flags & II_REACHED; }
+  void setReached() noexcept { flags |= II_REACHED; }
 };
 
 class Verifier {
@@ -43,9 +59,18 @@ private:
   void verifyPublicSymTab();
 
   void parse();
+  void parseFunction(const uint8_t *beginIp);
+
+  void enqueuePublicSymbols();
   /// \pre #ip is valid
   /// \pre currentOperandStackSize >= 0
-  void enqueue(const uint8_t *ip, int16_t currentOperandStackSize, bool jump);
+  void enqueueInst(const uint8_t *ip, int16_t currentOperandStackSize);
+  /// \pre #ip is valid
+  void enqueueFunction(const uint8_t *beginIp);
+  /// \pre #ip is valid
+  /// \pre nclosurevars >= 0
+  void enqueueClosure(const uint8_t *beginIp, int16_t nclosurevars);
+
   void parseAt(const uint8_t *ip);
 
   const uint8_t *lookUpIp(int32_t ioffset);
@@ -54,9 +79,16 @@ private:
   void verifyIp(int32_t ioffset);
   void verifyString(int32_t offset);
 
+  /// \pre #ip is valid
   int32_t ioffsetOf(const uint8_t *ip) { return ip - codeBegin; }
+  /// \pre #ip is valid
   InstInfo *instInfoOf(const uint8_t *ip) {
     return instInfo.get() + ioffsetOf(ip);
+  }
+
+  /// \pre #ip is valid and start of the function
+  FunctionInfo *functionInfoOf(const uint8_t *beginIp) {
+    return functionInfo.get() + ioffsetOf(beginIp);
   }
 
   friend class InstParser;
@@ -64,9 +96,18 @@ private:
 private:
   ByteFile &file;
   std::unique_ptr<InstInfo[]> instInfo;
-  std::vector<const uint8_t *> stack;
+  std::unique_ptr<FunctionInfo[]> functionInfo;
+  std::vector<const uint8_t *> instStack;
+  std::vector<const uint8_t *> functions;
+  size_t nextFunctionIdx;
   const uint8_t *const codeBegin;
   const uint8_t *const codeEnd;
+
+  struct {
+    int32_t nargs;
+    int32_t nlocals;
+    int32_t nclosurevars;
+  } currentFunction;
 };
 
 class InstParser {
@@ -87,7 +128,7 @@ private:
   uint8_t nextByte();
   int32_t nextSigned();
   const char *nextString();
-  const uint8_t *nextLabel();
+  const uint8_t *nextIp();
 
   void operandStackPop(int32_t k);
   void operandStackPush(int32_t k);
@@ -131,7 +172,7 @@ void InstParser::operandStackPush(int32_t k) {
   currentOperandStackSize += k;
 }
 
-const uint8_t *InstParser::nextLabel() {
+const uint8_t *InstParser::nextIp() {
   int32_t ioffset = nextSigned();
   return verifier.lookUpIp(ioffset);
 }
@@ -205,7 +246,7 @@ void InstParser::parse() {
     return;
   }
   case I_JMP: {
-    const uint8_t *ip = nextLabel();
+    const uint8_t *ip = nextIp();
     jumpTarget = ip;
     stop = true;
     return;
@@ -253,26 +294,57 @@ void InstParser::parse() {
   }
   case I_CJMPz:
   case I_CJMPnz: {
-    // TODO
-    jumpTarget = nextLabel();
+    jumpTarget = nextIp();
     operandStackPop(1);
     return;
   }
   case I_BEGIN:
   case I_BEGINcl: {
-    // TODO function
+    int32_t nargs = nextSigned();
+    if (nargs < 0) {
+      invalidByteFileError("negative nargs {} in (C)BEGIN", nargs);
+    }
+    if (nargs >= (1 << 16)) {
+      invalidByteFileError("too large nargs {} >= 2^16 in (C)BEGIN", nargs);
+    }
+    int32_t nlocals = nextSigned();
+    if (nlocals < 0) {
+      invalidByteFileError("negative nlocals {} in (C)BEGIN", nlocals);
+    }
+    verifier.currentFunction.nargs = nargs;
+    verifier.currentFunction.nlocals = nlocals;
     return;
   }
   case I_CLOSURE: {
-    // TODO
+    const uint8_t *closureIp = nextIp();
+    int32_t nclosurevars = nextSigned();
+    if (nclosurevars < 0) {
+      invalidByteFileError("negative closure vars num {} for CLOSURE {#:x}",
+                           nclosurevars, verifier.ioffsetOf(closureIp));
+    }
+    for (int i = 0; i < nclosurevars; ++i) {
+      // TODO
+      nextByte();
+      nextSigned();
+    }
+    verifier.enqueueClosure(closureIp, nclosurevars);
+    operandStackPush(1);
     return;
   }
   case I_CALLC: {
-    // TODO call
+    int32_t nargs = nextSigned();
+    if (nargs < 0) {
+      invalidByteFileError("negative nargs {} in CALLC", nargs);
+    }
+    operandStackPop(nargs + 1);
+    operandStackPush(1);
     return;
   }
   case I_CALL: {
-    // TODO call
+    const uint8_t *functionIp = nextIp();
+    nextSigned();
+    verifier.enqueueFunction(functionIp);
+    operandStackPush(1);
     return;
   }
   case I_TAG: {
@@ -348,6 +420,7 @@ void InstParser::parse() {
 
 Verifier::Verifier(ByteFile &file)
     : file(file), instInfo(new InstInfo[file.getCodeSizeBytes()]),
+      functionInfo(new FunctionInfo[file.getCodeSizeBytes()]),
       codeBegin(file.getCode()),
       codeEnd(file.getCode() + file.getCodeSizeBytes()) {}
 
@@ -378,45 +451,110 @@ const uint8_t *Verifier::lookUpIp(int32_t ioffset) {
 void Verifier::parseAt(const uint8_t *ip) {
   InstParser parser(ip, *this);
   if (parser.getJumpTarget()) {
-    enqueue(parser.getJumpTarget(), parser.getNextOperandStackSize(),
-            /*jump=*/true);
+    enqueueInst(parser.getJumpTarget(), parser.getNextOperandStackSize());
   }
-  if (parser.doesStop())
-    return;
-  enqueue(parser.getNextIp(), parser.getNextOperandStackSize(), /*jump=*/false);
+  if (!parser.doesStop()) {
+    enqueueInst(parser.getNextIp(), parser.getNextOperandStackSize());
+  }
 }
 
-void Verifier::enqueue(const uint8_t *ip, int16_t currentOperandStackSize,
-                       bool jump) {
+void Verifier::enqueueClosure(const uint8_t *beginIp, int16_t nclosurevars) {
+  if (*beginIp != I_BEGINcl) {
+    invalidByteFileError("a closure begins with bytecode {:#x}, "
+                         "expected CBEGIN ({:#x})",
+                         *beginIp, (int)I_BEGINcl);
+  }
+  FunctionInfo *info = functionInfoOf(beginIp);
+  if (info->isReached()) {
+    if (info->isNonClosure()) {
+      invalidByteFileError("function at {:#x} is both closure and non-closure",
+                           ioffsetOf(beginIp));
+    }
+    if (info->nclosurevars != nclosurevars) {
+      invalidByteFileError(
+          "inconsistent variable count ({} vs. {}) for closure at {:#x}",
+          info->nclosurevars, nclosurevars, ioffsetOf(beginIp));
+    }
+    return;
+  }
+  info->setReached();
+  info->setClosure();
+  info->nclosurevars = nclosurevars;
+  functions.push_back(beginIp);
+}
+
+void Verifier::enqueueFunction(const uint8_t *beginIp) {
+  if (*beginIp != I_BEGIN) {
+    invalidByteFileError("a (non-closure) function begins with bytecode {:#x}, "
+                         "expected BEGIN ({:#x})",
+                         *beginIp, (int)I_BEGIN);
+  }
+  FunctionInfo *info = functionInfoOf(beginIp);
+  if (info->isReached()) {
+    if (info->isClosure()) {
+      invalidByteFileError("function at {:#x} is both closure and non-closure",
+                           ioffsetOf(beginIp));
+    }
+    return;
+  }
+  info->setReached();
+  info->setNonClosure();
+  functions.push_back(beginIp);
+}
+
+void Verifier::enqueueInst(const uint8_t *ip, int16_t currentOperandStackSize) {
+  if (*ip == I_BEGIN || *ip == I_BEGINcl) {
+    invalidByteFileError("non-call reach to BEGIN/CBEGIN instruction at {:#x}",
+                         ioffsetOf(ip));
+  }
   InstInfo *info = instInfoOf(ip);
-  if (info->visited) {
+  if (info->isReached()) {
     if (info->operandStackSize != currentOperandStackSize) {
-      runtimeError(
+      invalidByteFileError(
           "operand stack size inconsistency at instruction {:#x}; {} vs. {}",
           ioffsetOf(ip), info->operandStackSize, currentOperandStackSize);
     }
     return;
   }
-  info->visited = true;
+  info->setReached();
   info->operandStackSize = currentOperandStackSize;
-  stack.push_back(ip);
+  instStack.push_back(ip);
 }
 
-void Verifier::parse() {
+void Verifier::enqueuePublicSymbols() {
   const int32_t *symtab = file.getPublicSymbolTable();
   for (int i = 0; i < file.getPublicSymbolNum(); ++i) {
     int32_t ioffset = symtab[2 * i + 1];
     const uint8_t *ip = lookUpIp(ioffset);
-    enqueue(ip, /*currentOperandStackSize=*/0, /*jump=*/true);
+    enqueueFunction(ip);
   }
-  while (!stack.empty()) {
-    const uint8_t *ip = stack.back();
-    stack.pop_back();
+}
+
+void Verifier::parseFunction(const uint8_t *beginIp) {
+  FunctionInfo *info = functionInfoOf(beginIp);
+  if (info->isClosure())
+    currentFunction.nclosurevars = info->nclosurevars;
+  instStack.push_back(beginIp);
+  while (!instStack.empty()) {
+    const uint8_t *ip = instStack.back();
+    instStack.pop_back();
     try {
       parseAt(ip);
     } catch (InvalidByteFileError &e) {
       invalidByteFileError("failed to parse at instruction {:#x}: {}",
                            ioffsetOf(ip), e.what());
+    }
+  }
+}
+
+void Verifier::parse() {
+  while (nextFunctionIdx < functions.size()) {
+    const uint8_t *beginIp = functions[nextFunctionIdx++];
+    try {
+      parseFunction(beginIp);
+    } catch (InvalidByteFileError &e) {
+      invalidByteFileError("in function {:#x}: {}", ioffsetOf(beginIp),
+                           e.what());
     }
   }
 }
